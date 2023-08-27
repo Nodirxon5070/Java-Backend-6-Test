be74f4e55900344d030fa9752fd8cf1b12953447
Nodirxon <nodirxon5070@gmail.com>
2023-08-27 17:40:56 +0500

    Start

---
+++ src/main/java/com/company/TestErd/TestErdApplication.java
@@ -0,0 +1,13 @@
+package com.company.TestErd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestErdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestErdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/TestErd/TestErdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.TestErd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestErdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
