42a7d0a33ce971e862e0bd5e93025476c3e76c86
Nodirxon <nodirxon5070@gmail.com>
2023-08-27 18:18:45 +0500

    Update

---
+++ src/main/java/com/company/TestErd/Service/Mapper/SubjectTeacherMapper.java
@@ -0,0 +1,9 @@
+package com.company.TestErd.Service.Mapper;
+
+import org.mapstruct.Mapper;
+
+import java.util.stream.Collectors;
+
+@Mapper(componentModel = "spring",imports = Collectors.class)
+public class SubjectTeacherMapper {
+}
