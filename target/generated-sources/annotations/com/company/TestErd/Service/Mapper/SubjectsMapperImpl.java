03dc04056f2a43f52efeb4752e1cf1cc8f5b47a4
Asadbek <asadbekkaramov08@gmail.com>
2023-08-31 23:22:22 +0500

    pom.xml add file

--- target/generated-sources/annotations/com/company/TestErd/Service/Mapper/SubjectsMapperImpl.java
+++ target/generated-sources/annotations/com/company/TestErd/Service/Mapper/SubjectsMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-29T10:03:37+0500",
+    date = "2023-08-29T16:21:32+0500",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 20.0.2 (Oracle Corporation)"
 )
 @Component
